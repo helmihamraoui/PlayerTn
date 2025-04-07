ef21b4660b8d54059bbbe8c3b8e1a08627187e1d
helmihamraoui <helmihamraoui54@gmail.com>
2025-04-07 13:52:19 +0100

    backend

--- server/server.js
+++ server/server.js
@@ -1,30 +1,49 @@
-import express from 'express';
-import http from 'http';
-import { Server as SocketIOServer } from 'socket.io';
+import express, { Router } from 'express';
+import dotenv from 'dotenv';
+import dbConnect from './config/dbConnect.js';
+import authRoutes from './routes/auth.routes.js';
+import cors from 'cors';
+import http from 'http'; // Import HTTP module to create server
+import msgRouter from './routes/chat.routes.js';
+import multer from 'multer';
+dotenv.config(); // Load environment variables
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000; // Ensure default port if none in .env
 
-const server = http.createServer(app);
-const io = new SocketIOServer(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
+const server = http.createServer(app); // Create HTTP server
+
+// Middleware
+app.use(express.json(), cors()); // Parse JSON requests
+
+// Connect to the database
+dbConnect();
+
+// Routes
+app.use('/api', authRoutes, Router, msgRouter); // Authentication, Recipe, and Chat routes
+
+app.use('/uploads', express.static('public/uploads'));
+console.log(`you are On server side port: ${PORT}`);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
     },
 });
 
-// Basic Socket.IO connection for testing
-io.on('connection', (socket) => {
-    console.log('User connected');
+const upload = multer({ storage: storage });
 
-    socket.on('test_event', (data) => {
-        console.log('Test event received:', data);
-        socket.emit('test_response', { message: 'Test response from server' });
-    });
+app.post('/api/upload', upload.single('sticker'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+    console.log(`File uploaded: ${req.file.filename}`);
+    const filePath = req.file.path.replace('public/', '');
+    res.status(200).send({ filePath });
 });
 
 // Start the server
